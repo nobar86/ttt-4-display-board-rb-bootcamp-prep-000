2644a28729dc0320af753436d1439b9b35768548
Michael Rabon <nobar86@gmail.com>
2020-08-12 12:30:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,8 @@
 def display_board(board)
   board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "]
   puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
