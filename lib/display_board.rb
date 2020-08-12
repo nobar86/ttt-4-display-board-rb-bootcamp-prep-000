e0f8ff741e66a6d182fc91f1f019c3fe78197de5
Michael Rabon <nobar86@gmail.com>
2020-08-12 12:33:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,9 +6,9 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 
-  puts "   |   |   "
-  puts "-----------"
   puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 
